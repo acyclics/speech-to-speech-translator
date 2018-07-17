d0e6454a57c8e4a1df286a74bd62e910610bdef2
Aslan <impeccableaslan@gmail.com>
2018-07-17 22:20:36 +0200

    Update translate.cpp

--- translate.cpp
+++ translate.cpp
@@ -59,20 +59,20 @@ FILE *fpipe = nullptr;
 FILE *fpipesec = nullptr;
 
 // path to pocketsphinx parameters must be adjusted
-char command[] = "pocketsphinx_continuous -inmic yes -adcdev plughw:1,0 -samprate 16000 -logfn /dev/null -hmm /home/pi/Desktop/spanish/cmusphinx-es-5.2/model_parameters/voxforge_es_sphinx.cd_ptm_4000 -lm /home/pi/Desktop/spanish/es-20k.lm.bin -dict /home/pi/Desktop/spanish/es.dict > foo.txt";
+char command[] = "pocketsphinx_continuous -inmic yes -adcdev plughw:1,0 -samprate 16000 -logfn /dev/null -hmm (hmm path) -lm (lm path) -dict (dict path) > (text file)";
 
 // enter api-key 
-char sendcurl[] = "curl --user apikey:Viqc9hPH10U8aIe_WX7iEmEsudzgYu6Z69_RLYthEQAt --request POST --header \"Content-Type: application/json\" --data \"{\\\"text\\\"\:";
+char sendcurl[] = "curl --user apikey:(your key) --request POST --header \"Content-Type: application/json\" --data \"{\\\"text\\\"\:";
 
 // url must be adjusted
-char endtext[] = "\\\"\model_id\\\"\:\\\"es-en\\\"\}\"\ \"\https://gateway.watsonplatform.net/language-translator/api/v3/translate?version=2018-05-01\"";
+char endtext[] = "\\\"\model_id\\\"\:\\\"es-en\\\"\}\"\ \"\(your URL)\"";
 
 char spanishstart[] = "[\\\"";
 char spanishend[] = "\\\"\],";
 char flitetoappend[] = "flite \"";
 
 // path to output wav file must be the same as path used for wav file input
-char flitetoend[] = "\" /home/pi/Desktop/hear.wav";
+char flitetoend[] = "\" (wav file)";
 
 char endofspeech[] = " end of speech";
 
@@ -174,18 +174,18 @@ bool alphabet(char value)
 
 int main()
 {
-	remove("/home/pi/Desktop/foo.txt");
-	std::thread recording(startbranch);                                       // starts pocketsphinx in a thread
-	sleep(5);                                                                 // sleep for 5 seconds on Linux/Unix systems for "foo.txt" to be created
-        system("pacmd set-card-profile bluez_card.70_26_05_AE_65_DD a2dp_sink");  // bluetooth address must be adjusted
-        system("pacmd set-default-sink bluez_sink.70_26_05_AE_65_DD.a2dp_sink");  // bluetooth address must be adjusted
+	remove("(text file)");
+	std::thread recording(startbranch);                                    	        // starts pocketsphinx in a thread
+	sleep(5);                                                                 	// sleep for 5 seconds on Linux/Unix systems for "foo.txt" to be created
+        system("pacmd set-card-profile bluez_card.(Bluetooth address) a2dp_sink");	// bluetooth address must be adjusted
+        system("pacmd set-default-sink bluez_sink.(Bluetooth address).a2dp_sink");      // bluetooth address must be adjusted
 	
-        std::ifstream myfile("/home/pi/Desktop/foo.txt");                         // reads file pocketsphinx outputs to
+        std::ifstream myfile("(text file)");                       		        // reads file pocketsphinx outputs to
 	std::string templine;
   
 	while(1)
 	{
-		myfile.clear();                                                   // ignore lines that are already read
+		myfile.clear();                                                	        // ignore lines that are already read
 		while(getline(myfile, templine))
 		{
 			char* sendspanish = nullptr;
@@ -193,21 +193,21 @@ int main()
 			char *flitecommand = nullptr;
       
 			std::string spanishstr[]{ templine, spanishend };
-			getPathsEX(sendspanish, spanishstart, 3, spanishstr, 2);  // stores syntax for word/words into "sendspanish"
+			getPathsEX(sendspanish, spanishstart, 3, spanishstr, 2);       // stores syntax for word/words into "sendspanish"
 			
                         std::string commandstr[]{ sendspanish, endtext };
-			getPathsEX(sendcommand, sendcurl, 139, commandstr, 2);    // stores command for requesting translation from IBM into "sendcommand"
+			getPathsEX(sendcommand, sendcurl, 139, commandstr, 2);         // stores command for requesting translation from IBM into "sendcommand"
 			
                         fpipesec = (FILE*)popen(sendcommand, "r");
 			char* a = new char[200];
 			char* b = nullptr;
 			int countingwords(0);
-			fread(a, 200, 1, fpipesec);                               // stores terminal output into "a"
+			fread(a, 200, 1, fpipesec);                                    // stores terminal output into "a"
       
 			for (int count(46); count < 200; ++count)
 			{
-				++countingwords;                                  // countingwords store the number of characters the word/words being translated contain
-				if (alphabet(a[count+1]))                         // if the next character is an alphabet or "space", keep looping. if it isn't an alphabet or "space", then we have finished calculating "countingwords", so end the loop
+				++countingwords;                                       // countingwords store the number of characters the word/words being translated contain
+				if (alphabet(a[count+1]))                              // if the next character is an alphabet or "space", keep looping. if it isn't an alphabet or "space", then we have finished calculating "countingwords", so end the loop
 				{
 				}
 				else
@@ -216,32 +216,32 @@ int main()
 				}
 			}
       
-			b = new char[countingwords + 15];                         // add 15 so we would have additional space to signify end of speech through the char array "endofspeech"
+			b = new char[countingwords + 15];                              // add 15 so we would have additional space to signify end of speech through the char array "endofspeech"
       
 			for (int count(0); count < countingwords; ++count)
 			{
-				b[count] = a[46 + count];                         // store the translated word/words into "b"
+				b[count] = a[46 + count];                              // store the translated word/words into "b"
 			}
       
                         for (int count(0); count < 14; ++count)
                         {
-                                b[countingwords + count] = endofspeech[count];    // append "end of speech" to "b"
+                                b[countingwords + count] = endofspeech[count];         // append "end of speech" to "b"
                         }
       
 			b[countingwords + 14] = '\0';
                         
 			std::string flitestr[]{ b, flitetoend };
-			getPathsEX(flitecommand, flitetoappend, 7, flitestr, 2);  // stores command for synthesizing speech to "flitecommand"
+			getPathsEX(flitecommand, flitetoappend, 7, flitestr, 2);       // stores command for synthesizing speech to "flitecommand"
 
-			remove("/home/pi/Desktop/hear.wav");
-			system(flitecommand);                                     // creates wav file that contains the speech for the translated word/words
+			remove("(wav file)");
+			system(flitecommand);                                          // creates wav file that contains the speech for the translated word/words
       
-			while (!(exists("/home/pi/Desktop/hear.wav")))	          // if the wav file is not created yet, sleep until it is created
+			while (!(exists("(wav file)")))	         		       // if the wav file is not created yet, sleep until it is created
                         {
                                 sleep(1);
                         }
                         
-			system("omxplayer -o alsa /home/pi/Desktop/hear.wav"); 	// sound player must be changed depending on system (omxplayer for linux)	
+			system("omxplayer -o alsa (wav file)"); 		       // sound player must be changed depending on system (omxplayer for linux)	
 
 			pclose(fpipesec);
 			delete[] a;
@@ -250,7 +250,7 @@ int main()
 			delete[] sendspanish;
 			delete[] flitecommand;
 		}
-		sleep(1);                                                         // reduces burden on CPU due to while(1) statements
+		sleep(1);                                                              // reduces burden on CPU due to while(1) statements
 	}
 	pclose(fpipe);
 	recording.join();
